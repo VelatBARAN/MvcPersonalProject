6c040212c69c6d4f85ccf9f97840fa79e2f23c7c
welat baran <okyanusmavisi1990@hotmail.com>
2020-12-14 14:21:19 +0300

    Message send was done

--- MvcPersonalProject.Common/Helpers/MailHelper.cs
+++ MvcPersonalProject.Common/Helpers/MailHelper.cs
@@ -53,5 +53,6 @@ namespace MvcPersonalProject.Common.Helpers
 
             return result;
         }
+
     }
 }
--- MvcPersonalProject.UI/Controllers/HomeController.cs
+++ MvcPersonalProject.UI/Controllers/HomeController.cs
@@ -1,9 +1,11 @@
 ﻿using MvcPersonalProject.BLL;
+using MvcPersonalProject.Common.Helpers;
 using MvcPersonalProject.Entity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Net.Mail;
 using System.Web;
 using System.Web.Mvc;
 
@@ -30,6 +32,28 @@ namespace MvcPersonalProject.UI.Controllers
                 return HttpNotFound();
             }
             return View(projectDone);
+        }
+
+        [HttpPost]
+        [ValidateInput(false)]
+        public ActionResult MessageSend(string name=null,string email=null,string subject=null,string message = null)
+        {
+            MailMessage mail = new MailMessage();
+            mail.To.Add("baranvelat021@gmail.com");
+            mail.From = new MailAddress(email);
+            mail.Subject = "Welat BARAN Kişisel Web Sayfası - Gelen Mesaj";
+            string Body = $"Merhaba ben {name} <br><br> Email : {email} <br><br> Konu : {subject} <br> <br> Mesaj İçeriği : {message}";
+            mail.Body = Body;
+            mail.IsBodyHtml = true;
+            SmtpClient smtp = new SmtpClient();
+            smtp.Host = "smtp.gmail.com";
+            smtp.Port = 587;
+            smtp.UseDefaultCredentials = false;
+            smtp.Credentials = new System.Net.NetworkCredential("baranvelat021@gmail.com", "liceli21?"); // Enter seders User name and password  
+            smtp.EnableSsl = true;
+            smtp.Send(mail);
+            ViewBag.Info = "Mesajınız başarıyla gönderilmiştir.";
+            return View();
         }
     }
 }
